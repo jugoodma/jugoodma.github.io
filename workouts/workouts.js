8b7cee988e882ac25dede6029def60b94ffbd3c3
Justin <jugoodma@users.noreply.github.com>
2020-01-16 01:22:55 -0500

    workouts database

---
+++ workouts/workouts.js
@@ -0,0 +1,23 @@
+var data =
+
+[
+	{
+		"workout": "10min warm up\n4-10 2min uphill run\nbump to 3-7 3min",
+		"tags": ["run", "outdoor", "hills", "vO2max"]
+	},
+	{
+		"workout": "10min warm up\n4-5x1000m 1mi race pace\n 2-3min recovery",
+		"tags": ["run", "track", "vO2max"]
+	},
+	{
+		"workout": "The Escalator...",
+		"link": "https://youtu.be/8w-FuRMilH4",
+		"tags": ["bike", "indoor", "ftp"]
+	},
+	{
+		"workout": "Tempo: Aerobic Building, Ride 2\n- 15 minutes easy riding, 57-75% FTP or 69-83% threshold HR\n- 2 x 20 minutes, 10 min rest intervals, at sweetspot, 88-93% FTP or 92 to 98% of threshold HR\n- 1hr - cruise but include 4-6 Vo2 intervals of 3 minutes with 3 minute rest, cruise at 80-85% FTP or 85-91% T.HR, Vo2 115% FTP or 105+% T.HR\n- 30min-1.5 hour endurance/tempo, 70-85% FTP or 69-75% T.HR\n- 15min easy riding, 57-75% FTP or 69-83% threshold HR",
+		"tags": ["bike", "outdoor", "aerobic"]
+	}
+]
+
+;
