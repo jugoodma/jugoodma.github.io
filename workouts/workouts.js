66ca51c4245bf4abb948b90063b908344b9b01a5
Justin <jugoodma@users.noreply.github.com>
2020-01-16 17:41:42 -0500

    workout changes:

--- workouts/workouts.js
+++ workouts/workouts.js
@@ -10,13 +10,46 @@ var data =
 		"tags": ["run", "track", "vO2max"]
 	},
 	{
-		"workout": "The Escalator...",
+		"workout": "The Escalator\n",
 		"link": "https://youtu.be/8w-FuRMilH4",
 		"tags": ["bike", "indoor", "ftp"]
 	},
 	{
 		"workout": "Tempo: Aerobic Building, Ride 2\n- 15 minutes easy riding, 57-75% FTP or 69-83% threshold HR\n- 2 x 20 minutes, 10 min rest intervals, at sweetspot, 88-93% FTP or 92 to 98% of threshold HR\n- 1hr - cruise but include 4-6 Vo2 intervals of 3 minutes with 3 minute rest, cruise at 80-85% FTP or 85-91% T.HR, Vo2 115% FTP or 105+% T.HR\n- 30min-1.5 hour endurance/tempo, 70-85% FTP or 69-75% T.HR\n- 15min easy riding, 57-75% FTP or 69-83% threshold HR",
 		"tags": ["bike", "outdoor", "aerobic"]
+	},
+	{
+		"workout": "ftp test\n10min ez\n5min all out\n10min recovery\n20min all out\n\n(20min effort avg power) * 0.95 == 1hr ftp",
+		"tags": ["bike", "ftp", "test", "outdoor", "indoor"]
+	},
+	{
+		"workout": "20min warm up\n5x4min 80-85-90-85-80% pyramid\n4min recovery\n5x4min 80-85-90-85-80% pyramid",
+		"tags": ["bike", "ftp", "outdoor", "indoor"]
+	},
+	{
+		"workout": "30-min HIIT\n- 5 minute warm up\n- 8x30sec efforts\n 3-1.5min recovery\n- 6 minute cool down",
+		"link": "https://youtu.be/4v8heR3bSj4",
+		"tags": ["bike", "hiit", "indoor"]
+	},
+	{
+		"workout": "Drill, Swim, Sprint - Butterfly Variant\n---\n1x500 warm up\n5x100 free descend\n4x75 50 drill 25 swim\n4x50 kick\n---\n8x50 fly drill\n8x50 fly swim\n4x50 fly sprint\n---\n3x100 fly kick\n---\n4x{75 IM, 25 fly kick} drop least favorite stroke\n---\n1x300 warm down",
+		"link": "https://www.swimworkouts.net/workouts/112",
+		"tags": ["swim", "indoor"]
+	},
+	{
+		"workout": "warm up\n10x{50 sprint, 50 drill, 50 kick} fly\ncool down",
+		"tags": ["swim", "indoor"]
+	},
+
+	{
+		"workout": "Link",
+		"link": "http://100swimmingworkouts.com/",
+		"tags": ["swim", "util"]
+	},
+	{
+		"workout": "Link",
+		"link": "https://www.youtube.com/watch?v=PpbZ97_7Ofs&list=PLUdAMlZtaV13e3-rJq1W0O1kf2DVlslmC",
+		"tags": ["bike", "util"]
 	}
 ]
 
