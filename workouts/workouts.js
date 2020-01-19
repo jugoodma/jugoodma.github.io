544091e8260f1a8a2b5ef8d60609971becf090a4
Justin <jugoodma@users.noreply.github.com>
2020-01-18 22:14:29 -0500

    update workouts

--- workouts/workouts.js
+++ workouts/workouts.js
@@ -10,12 +10,22 @@ var data =
 		"tags": ["run", "track", "vO2max"]
 	},
 	{
-		"workout": "The Escalator\n",
+		"workout": "10min warm up\n12-20x 30sec hard 30sec ez\nbump to 6-10x 60/60",
+		"tags": ["run", "outdoor", "vO2max"]
+	},
+	{
+		"workout": "1mi warm up\n1-2-3-4-3-2-1 min pyramid @ 5k pace\n2min recovery\ncool down as needed",
+		"tags": ["run", "outdoor"]
+	},
+	{
+		"title": "The Escalator",
+		"workout": "time rpm e ftp%\n3:00  80 1 ez\n2:00  90 2  60%\n1:00 100 4  70%\n\n1:30  80 2  60%\n1:30  90 5  80%\n1:00  95 6  85%\n\n0:30  85 4  75%\n1:30  80 7  85%\n1:00  90 7  90%\n\n0:30  85 5  80%\n1:30 100 7  90%\n1:00 100 7  95%\n\n0:30  80 6  85%\n1:30  90 7  95%\n1:00  90 8 100%\n\n0:30  90 7  90%\n1:30  95 8 100%\n1:00 100 9 105%\n\n1:30  80 1 ez\n\n1:30  85 8  90%\n1:00  90 8  95%\n1:30  95 8  95%\n1:00 100 9 100%\n\n1:30  80 1 ez",
 		"link": "https://youtu.be/8w-FuRMilH4",
 		"tags": ["bike", "indoor", "ftp"]
 	},
 	{
-		"workout": "Tempo: Aerobic Building, Ride 2\n- 15 minutes easy riding, 57-75% FTP or 69-83% threshold HR\n- 2 x 20 minutes, 10 min rest intervals, at sweetspot, 88-93% FTP or 92 to 98% of threshold HR\n- 1hr - cruise but include 4-6 Vo2 intervals of 3 minutes with 3 minute rest, cruise at 80-85% FTP or 85-91% T.HR, Vo2 115% FTP or 105+% T.HR\n- 30min-1.5 hour endurance/tempo, 70-85% FTP or 69-75% T.HR\n- 15min easy riding, 57-75% FTP or 69-83% threshold HR",
+		"title": "Tempo: Aerobic Building, Ride 2",
+		"workout": "- 15 minutes easy riding, 57-75% FTP or 69-83% threshold HR\n- 2 x 20 minutes, 10 min rest intervals, at sweetspot, 88-93% FTP or 92 to 98% of threshold HR\n- 1hr - cruise but include 4-6 Vo2 intervals of 3 minutes with 3 minute rest, cruise at 80-85% FTP or 85-91% T.HR, Vo2 115% FTP or 105+% T.HR\n- 30min-1.5 hour endurance/tempo, 70-85% FTP or 69-75% T.HR\n- 15min easy riding, 57-75% FTP or 69-83% threshold HR",
 		"tags": ["bike", "outdoor", "aerobic"]
 	},
 	{
@@ -23,16 +33,19 @@ var data =
 		"tags": ["bike", "ftp", "test", "outdoor", "indoor"]
 	},
 	{
+		"title": "2x20 ftp blocks",
 		"workout": "20min warm up\n5x4min 80-85-90-85-80% pyramid\n4min recovery\n5x4min 80-85-90-85-80% pyramid",
 		"tags": ["bike", "ftp", "outdoor", "indoor"]
 	},
 	{
-		"workout": "30-min HIIT\n- 5 minute warm up\n- 8x30sec efforts\n 3-1.5min recovery\n- 6 minute cool down",
+		"title": "30-min HIIT",
+		"workout": "- 5 minute warm up\n- 8x30sec efforts\n 3-1.5min recovery\n- 6 minute cool down",
 		"link": "https://youtu.be/4v8heR3bSj4",
 		"tags": ["bike", "hiit", "indoor"]
 	},
 	{
-		"workout": "Drill, Swim, Sprint - Butterfly Variant\n---\n1x500 warm up\n5x100 free descend\n4x75 50 drill 25 swim\n4x50 kick\n---\n8x50 fly drill\n8x50 fly swim\n4x50 fly sprint\n---\n3x100 fly kick\n---\n4x{75 IM, 25 fly kick} drop least favorite stroke\n---\n1x300 warm down",
+		"title": "Drill, Swim, Sprint - Butterfly Variant",
+		"workout": "1x500 warm up\n5x100 free descend\n4x75 50 drill 25 swim\n4x50 kick\n---\n8x50 fly drill\n8x50 fly swim\n4x50 fly sprint\n---\n3x100 fly kick\n---\n4x{75 IM, 25 fly kick} drop least favorite stroke\n---\n1x300 warm down",
 		"link": "https://www.swimworkouts.net/workouts/112",
 		"tags": ["swim", "indoor"]
 	},
