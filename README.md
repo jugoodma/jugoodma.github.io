f7ae3243dd8d5b705be7373847183edd7bcc718b
Justin Goodman <jugoodma@users.noreply.github.com>
2023-05-19 20:44:37 -0400

    rm terpconnect

--- README.md
+++ README.md
@@ -4,8 +4,6 @@
 
 <https://jugoodma.github.io>
 
-<https://terpconnect.umd.edu/~jugoodma>
-
 ```
 npx prettier --end-of-line lf --write .
 ```
--- gen.sh
+++ gen.sh
@@ -2,6 +2,6 @@
 
 (cd docs && python3 gen.py)
 
-(cd blog && python3 _site/create.py)
+(cd blog && python3 _site/bin/create.py)
 
 npx prettier --end-of-line lf --write .
